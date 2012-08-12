99f77296f0710b13aa26274031457b44990524ee
Joel Kraut <joel@spacemanlabs.com>
2012-08-11 17:23:46 -0700

    add a new target for a cli app to run this thing

---
+++ src/glsl/xcode/glsl-optimizer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  glsl-optimizer
+//
+//  Created by jkraut on 8/11/12.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
